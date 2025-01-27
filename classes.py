84be3c7022b68cac1e429488cfbc4264edac2149
natkor99 <pracamgrnk@gmail.com>
2025-01-27 16:38:36 +0100

    .

--- classes.py
+++ classes.py
@@ -4,3 +4,10 @@ class PoraRoku:
         self.temperatura = temperatura
         self.opis = opis
         self.typowe_zjawiska = typowe_zjawiska
+
+    def __str__(self):
+        return (f"Pora roku: {self.nazwa}"
+                f"Temperatura: {self.temperatura}"
+                f"Opis: {self.opis}"
+                f"Typowe zjawiska: {self.typowe_zjawiska}"
+                )
