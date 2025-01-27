dab7e1daf5d335291fda8dcc5ba71bca6732e3ff
natkor99 <pracamgrnk@gmail.com>
2025-01-27 16:46:58 +0100

    class

--- classes.py
+++ classes.py
@@ -1,1 +1,12 @@
 class PoraRoku:
+    def __init__(self, nazwa, temperatura, opis, typowe_zjawiska):
+        self.nazwa = nazwa
+        self.temperatura = temperatura
+        self.opis = opis
+        self.typowe_zjawiska = typowe_zjawiska
+
+    def __str__(self):
+        return (f"Pora roku: {self.nazwa}"
+                f"Temperatura: {self.temperatura}"
+                f"Opis: {self.opis}"
+                f"Typowe zjawiska: {', '.join(self.typowe_zjawiska)}")
