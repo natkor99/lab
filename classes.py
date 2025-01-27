6ba4c7140e5993a105ab101c8d99825627278879
natkor99 <pracamgrnk@gmail.com>
2025-01-27 16:33:28 +0100

    .

--- classes.py
+++ classes.py
@@ -1,1 +1,6 @@
 class PoraRoku:
+    def __init__(self, nazwa, temperatura, opis, typowe_zjawiska):
+        self.nazwa = nazwa
+        self.temperatura = temperatura
+        self.opis = opis
+        self.typowe_zjawiska = typowe_zjawiska
